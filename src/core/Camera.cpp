35b651ad50dfa6133b1ed3c3a3c89c3d89856a5e
Nikhil <nikhilchouhan2002@gmail.com>
2025-10-23 23:04:46 +0530

    changed camera default dir and pos

--- src/core/Camera.cpp
+++ src/core/Camera.cpp
@@ -12,9 +12,9 @@ Camera CreatePerspectiveCamera(PersCameraDesc cameraDesc)
 	//camera._up = camera._target.Cross(SM::Vector3(1.,0.,0.));
 	//camera._up.Normalize();
 
-	camera._pos = SM::Vector3(0.f, 10.f, -20.f);
+	camera._pos = SM::Vector3(-1.9467199, 2.5605016, -0.3993596);
 
-	SM::Vector3 lookAtPoint = SM::Vector3(0.f, 0.f, 0.f);
+	SM::Vector3 lookAtPoint = SM::Vector3(-5.9500113, 1.7869439, -0.468321);
 	camera._target = lookAtPoint - camera._pos;
 
 	camera._up = SM::Vector3(0.f, 1.f, 0.f);
