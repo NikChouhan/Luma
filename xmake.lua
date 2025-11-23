a1705f357c739322479174ac5830d687edcc32b0
Nikhil <nikhilchouhan2002@gmail.com>
2025-11-23 15:40:24 +0530

    xmake auto create compile commands file

--- xmake.lua
+++ xmake.lua
@@ -8,7 +8,7 @@ add_rules("mode.debug", "mode.release")
 add_defines("UNICODE", "_UNICODE", "WIN32_LEAN_AND_MEAN", "NOMINMAX")
 set_languages("cxx23", "c17")
 add_syslinks("user32.lib", "kernel32.lib", "shell32.lib", "comctl32.lib", "d3d12.lib", "dxgi.lib", "dxguid.lib")
-
+add_rules("plugin.compile_commands.autoupdate", {outputdir = ".vscode"})
 if (is_mode("debug")) then
     set_symbols("debug")
     add_defines("DEBUG")
